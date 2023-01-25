2168a924cf2b83d6743f1a7d4f00945a397f742e
s-caluk <seher.caluk@gmail.com>
2023-01-26 00:12:59 +0100

    day02

--- day02_select.sql
+++ day02_select.sql
@@ -201,6 +201,95 @@ SELECT id, isim
 FROM personel
 WHERE maas = 7000 OR isim = 'Ayse';
 
+/*-------------------------------------------------------------------------
+Q11: Id'si 10001, 10005 ya da 10008 olan personel isimlerini listeleyin
+---------------------------------------------------------------------------*/
+SELECT isim
+FROM personel
+WHERE id = '10001' OR id = '10005' OR id = '10008';
+
+/*================================== SELECT-IN ===========================================
+IN birden fazla mantiksal ifade ile tanimlayabilecegimiz durumlari tek komutla yazabilme
+imkani verir. IN icerisine yazacagimiz sartlar ayni field'a ait olmali.
+-----Syntax-----
+SELECT field1,field2,...
+FROM table_name
+WHERE field_name IN (deger1,deger2,...);
+=========================================================================================*/
+/*-------------------------------------------------------------------------
+Q12: Id'si 10001, 10005 ya da 10008 olan personel isimlerini listeleyin
+---------------------------------------------------------------------------*/
+SELECT isim
+FROM personel
+WHERE id 
+IN ('10001','10005','10008');
+/*-------------------------------------------------------------------------
+Q13: Maasi sadece 7000 veya 12000 olan personelin bilgilerini listele
+---------------------------------------------------------------------------*/
+SELECT *
+FROM personel
+WHERE maas IN (7000,12000);
+/*================================== SELECT-LIKE =============================================
+LIKE keyword sorgulama yaparken belirli patternleri kullanabilmemize olanak saglar.
+-----Syntax-----
+ SELECT field1,field2,...
+ FROM table_name
+ WHERE field LIKE pattern
+ 
+ ----------------
+ Pattern icin :
+ % ---> 0 veya daha fazla karakteri belirtir
+ _ ---> Tek bir karakteri temsil eder.
+=========================================================================================*/
+/*-------------------------------------------------------------------------
+Q14: Ismi A ile baslayan personeli listeleyiniz
+---------------------------------------------------------------------------*/
+SELECT *
+FROM personel
+WHERE isim LIKE 'a%';  -- ile baslasin sonrasi önemi degil!
+/*-------------------------------------------------------------------------
+Q15: Isminde Ayse olan personelin isimlerini listeleyiniz
+---------------------------------------------------------------------------*/
+SELECT *
+FROM personel
+WHERE isim LIKE '%ayse%';  -- önünde arkasinda olanlar önemli degil
+/*-------------------------------------------------------------------------
+Q16: Ismi n harfi ile biten personeli listeleyiniz
+---------------------------------------------------------------------------*/
+SELECT *
+FROM personel
+WHERE isim LIKE '%n';
+/*-------------------------------------------------------------------------
+Q17: Isminin 2. harfi e olan personeli listeleyiniz
+---------------------------------------------------------------------------*/
+SELECT *
+FROM personel
+WHERE isim LIKE '_e%';
+/*-------------------------------------------------------------------------
+Q18: Isminin 2. harfi e olup diger harflerinde y olan personeli listeleyiniz
+---------------------------------------------------------------------------*/
+SELECT *
+FROM personel
+WHERE isim LIKE '_e%' AND isim LIKE '%y%';
+/*-------------------------------------------------------------------------
+Q19: Isminde a harfi olmayan personeli listeleyiniz
+---------------------------------------------------------------------------*/
+SELECT *
+FROM personel
+WHERE isim NOT LIKE '%a%';
+/*-------------------------------------------------------------------------
+Q20: Maasi 5 haneli olan personeli listeleyiniz
+---------------------------------------------------------------------------*/
+SELECT *
+FROM personel
+WHERE maas LIKE '_____';
+/*-------------------------------------------------------------------------
+Q21: 1. harfi a ve 7. harfi a olan personeli listeleyiniz
+---------------------------------------------------------------------------*/
+SELECT *
+FROM personel
+WHERE isim LIKE 'a____a%';
+
 
 
 
